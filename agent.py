49c8073535a7ef26cb172506dd3e4bddc69439d8
Kevin Stanton <kevin@sproutsocial.com>
2025-04-01 00:10:37 -0500

    update agent prompt to avoid fetch tool calls attempting other websites that fail frequently and make responses take significantly longer. also prompt it to not answer questions about weather outside the US cuz national weather service doesnt support it.

--- agent.py
+++ agent.py
@@ -6,7 +6,15 @@ fast = FastAgent("FastAgent Example")
 
 # Define the agent
 @fast.agent(
-    instruction="You are a helpful AI Agent",
+    instruction="You are a helpful AI Agent that is able to look up locations in the " +
+                "United States by fetching latitude and longitude coordinates for " +
+                "locations and using that to call the weather tool to answer questions " +
+                "about the weather of various locations. When fetching latitude and " +
+                "longitude for a location, try wikipedia first. If you're asked for a location" +
+                "that is not in the United States, you should respond with a message saying " +
+                "that the location is outside the United States and you can't help with " +
+                "that. Always answer as concisely as possible, if you can answer the question" +
+                "in a single sentence, do so.",
     servers=[
         "fetch",
         "weather"
